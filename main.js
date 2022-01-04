093e7e545a6c475c834de0866acc94c22003fa16
Godson Thomas <godson.aocmusic@gmail.com>
2022-01-04 15:09:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const nav = document.querySelector("nav");
+const mobileNav = document.querySelector("nav.mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 60) {
+    nav.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled");
+  }
+});
+
+menuIcon.addEventListener("click", () => {
+  mobileMenuContainer.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  mobileMenuContainer.classList.remove("active");
+});
